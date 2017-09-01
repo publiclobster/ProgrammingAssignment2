477f4a64c23a00802f16299c12aa08c46b4351bd
publiclobster <paul.boehm@fluor.com>
2017-09-01 15:16:31 -0500

    this is my submission for the week

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the idea is that my first function will create a matrix object 
+## which can chache its inverse
+## The second function will compute the inverse of makeCacheMatrix() if the
+## inverse has not all ready been calculated. Otherwise it will retieve the 
+## invers from the chace.
 
-## Write a short comment describing this function
+## @z: a square invertible matrix
+  ## return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is used as the input to cacheSolve()
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(z = matrix()) {
+  
+  inv = NULL
+  set = function(y) {
+    z <<- y
+    inv <<- NULL
+  }
+  get = function() z
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## z is the output from makeCacheMatrix formula
+## in the following I'll return the input to makeCacheMatrix() matrixs' inverse 
+## note: if the inverse has already been calculated, it will take from cache 
+## and will not calculate inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(z, ...) {
+  inv = z$getinv()
+  
+  
+  if (!is.null(inv)){
+    message("retrieving previously cached data")
+    return(inv)
+  }
+  
+  # oterwise inverse calculation occure here 
+  mat.data = z$get()
+  inv = solve(mat.data, ...)
+  
+  # using the simple setinv function i'm setting the inverse.
+  z$setinv(inv)
+  
+  return(inv)
 }
